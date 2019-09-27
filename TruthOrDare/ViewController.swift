43b27b604edb1da2cd7f50fdd00fc36e918a0c42
Mayank <mayank.jnumca07@gmail.com>
2019-09-27 16:00:12 +0530

    fgjhgj

--- TruthOrDare/ViewController.swift
+++ TruthOrDare/ViewController.swift
@@ -17,14 +17,14 @@ class ViewController: UIViewController {
         let button = UIButton(type: .roundedRect)
         button.frame = CGRect(x: 20, y: 50, width: 100, height: 30)
         button.setTitle("Crash", for: [])
-//        button.addTarget(self, action: #selector(self.crashButtonTapped(_:)), for: .touchUpInside)
+        button.addTarget(self, action: #selector(self.crashButtonTapped(_:)), for: .touchUpInside)
         view.addSubview(button)
 
     }
 
-    @IBAction func crashButtonTapped(_ sender: AnyObject) {
-//        Crashlytics.sharedInstance().crash()
-    }
+//    @IBAction func crashButtonTapped(_ sender: AnyObject) {
+////        Crashlytics.sharedInstance().crash()
+//    }
 
 
 
