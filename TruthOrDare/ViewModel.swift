3845cf642ed6370e6ec9333e3505d4e723cc8a93
Mayank <mayank.jnumca07@gmail.com>
2019-09-30 12:35:26 +0530

    dfg

--- TruthOrDare/ViewModel.swift
+++ TruthOrDare/ViewModel.swift
@@ -13,20 +13,20 @@ class ViewModel {
     //Truth
     private let truths = [
         "What are you most self-conscious about?",
-//        "What would you do if you were the opposite sex for a month?",
-//        "What is the most expensive thing you have stolen?",
-//        "What is the most childish thing you still do?",
-//        "Have you ever let someone take the blame for something you did?"
+        "What would you do if you were the opposite sex for a month?",
+        "What is the most expensive thing you have stolen?",
+        "What is the most childish thing you still do?",
+        "Have you ever let someone take the blame for something you did?"
         
     ]
     
     //Dare
     private let dares = [
         "For a guy, put on makeup. For a girl, wash off your make up.",
-//        "Dance with no music for 1 minute.",
-//        "Try to drink a glass water while standing on your hands.",
-//        "Let the group pose you in an embarrassing position and take a picture.",
-//        "Curse like sailor for 20 seconds straight."
+        "Dance with no music for 1 minute.",
+        "Try to drink a glass water while standing on your hands.",
+        "Let the group pose you in an embarrassing position and take a picture.",
+        "Curse like sailor for 20 seconds straight."
     ]
     
     var totalTruths: Int { return truths.count }
